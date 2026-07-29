7de5d5eaaf93c4776c36f341c82eea136d6887a7
sunnysurya995-ctrl <sunny.surya995@gmail.com>
2026-07-29 18:15:47 -0400

    Update env.py

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -110,7 +110,7 @@ async def alembic_get_connection():
         user=config_service.DB_USER,
         password=config_service.DB_PASS,
         db=config_service.DB_NAME,
-        ip_type=IPTypes.PUBLIC,
+        ip_type=IPTypes.PRIVATE,
     )
     return conn
 
